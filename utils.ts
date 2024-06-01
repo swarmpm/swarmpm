a29571015bd3fd40d7ec4f8294418102b4a1aace
v1rtl <hi@v1rtl.site>
2024-06-01 18:46:11 +0200

    wip update readDir

--- utils.ts
+++ utils.ts
@@ -1,13 +1,22 @@
 import { walk } from "https://deno.land/std@0.224.0/fs/walk.ts";
+import * as mantaray from "https://esm.sh/mantaray-js@1.0.3";
 
 export const readDir = async (dir = ".") => {
   const files: File[] = [];
-  for await (const entry of walk(dir, { match: [/^(?!\.).+/] })) {
-    if (entry.isFile) {
-      const blob = await Deno.readFile(entry.path);
-      const file = new File([blob], entry.name);
-      files.push(file);
-    }
+  for await (
+    const entry of walk(dir, { includeDirs: false, skip: [/^.*\/\.[^/]*$/] })
+  ) {
+    const blob = await Deno.readFile(entry.path);
+    const file = new File([blob], entry.name);
+    files.push(file);
   }
   return files;
+};
+
+export const readManifest = (cid: string, filePath: string) => {
+  // const searchResult = await this.getDeserializedNode(hash, path);
+};
+
+export const loadDelegatePrivateKeyFromEnv = () => {
+  // ...
 };
